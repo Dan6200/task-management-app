abb22f87b383591f3809553d2dd172a0d885652d
Daniel Nyong <danielnyong66@gmail.com>
2024-04-24 10:19:48 +0100

    updated next, added clerk auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,15 @@
 import { Inter } from "next/font/google";
 import "@/styles/globals.css";
 import { StoreProvider } from "@/stores/StoreProvider";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+import { Button } from "@/components/ui/Button";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,12 +24,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={`bg-slate-100 ${inter.className}`}>
-        <StoreProvider>
-          <main className="container">{children}</main>
-        </StoreProvider>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`bg-slate-100 ${inter.className}`}>
+          <Navbar />
+          <StoreProvider>
+            <main className="container">{children}</main>
+          </StoreProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,20 @@
+import { SignedOut, SignInButton, SignedIn, UserButton } from "@clerk/nextjs";
+import { Button } from "./ui/Button";
+
+const Navbar = () => (
+  <header className="flex justify-between p-4 mb-8 px-16 border-b-2">
+    <h1 className="font-bold text-xl">Task Manager</h1>
+    <div>
+      <SignedOut>
+        <Button>
+          <SignInButton />
+        </Button>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </div>
+  </header>
+);
+
+export default Navbar;

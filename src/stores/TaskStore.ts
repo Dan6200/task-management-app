fdc35abc227a983b9bc8d37b748c802a6242b369
Daniel Nyong <danielnyong66@gmail.com>
2024-04-24 17:35:18 +0100

    added firestore, updated README

--- src/stores/TaskStore.ts
+++ src/stores/TaskStore.ts
@@ -1,4 +1,15 @@
+//cspell:ignore firestore
 import { types, Instance, SnapshotIn, onSnapshot } from "mobx-state-tree";
+import {
+  collection,
+  addDoc,
+  getDocs,
+  doc,
+  updateDoc,
+  deleteDoc,
+} from "firebase/firestore";
+import db from "./db";
+import { useClerk } from "@clerk/nextjs";
 
 export const TaskModel = types.model("Task", {
   id: types.identifier,
@@ -6,6 +17,8 @@ export const TaskModel = types.model("Task", {
   description: types.string,
   status: types.string,
 });
+
+const { user } = useClerk();
 
 export const TaskStore = types
   .model("TaskStore", {
@@ -15,6 +28,9 @@ export const TaskStore = types
     return {
       addTask(task: SnapshotIn<typeof TaskModel> | Instance<typeof TaskModel>) {
         self.tasks.push(task);
+        if (user) {
+          addDoc(collection(db, "users", user.id, "tasks"), task);
+        }
       },
       editTask(
         id: string,
@@ -25,33 +41,56 @@ export const TaskStore = types
           task.title = editedTask.title;
           task.description = editedTask.description;
           task.status = editedTask.status;
+          if (user) {
+            updateDoc(doc(db, "users", user.id, "tasks", id), editedTask);
+          }
         }
       },
       deleteTask(taskId: string) {
         const taskIndex = self.tasks.findIndex((task) => task.id === taskId);
         if (taskIndex !== -1) {
           self.tasks.splice(taskIndex, 1);
+          if (user) {
+            deleteDoc(doc(db, "users", user.id, "tasks", taskId));
+          }
         }
       },
     };
   });
 
+// If logged in in fetch tasks from Firestore under the current user's document and create the task store
 // Parse the tasks from local storage and create the task store
 let tasksFromLocalStorage: any = [];
+let tasksFromFirestore: any = [];
 if (typeof window !== "undefined") {
-  const tasksJSON = localStorage.getItem("taskStore");
-  if (tasksJSON) {
-    try {
-      tasksFromLocalStorage = JSON.parse(tasksJSON).tasks;
-    } catch (error) {
-      console.error("Error parsing tasks from local storage:", error);
+  if (user == null) {
+    const tasksJSON = localStorage.getItem("taskStore");
+    if (tasksJSON) {
+      try {
+        tasksFromLocalStorage = JSON.parse(tasksJSON).tasks;
+      } catch (error) {
+        console.error("Error parsing tasks from local storage:", error);
+      }
     }
+  } else {
+    getDocs(collection(db, "users", user.id, "tasks")).then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        tasksFromFirestore.push(doc.data());
+      });
+    });
   }
 }
 
-export let taskStore = TaskStore.create({
-  tasks: tasksFromLocalStorage,
-});
+let taskStore = null;
+if (user) {
+  taskStore = TaskStore.create({
+    tasks: tasksFromFirestore,
+  });
+} else {
+  taskStore = TaskStore.create({
+    tasks: tasksFromLocalStorage,
+  });
+}
 
 // Function to update the store with the initial snapshot
 export function updateTaskStoreWithSnapshot(snapshot: any) {
@@ -59,8 +98,14 @@ export function updateTaskStoreWithSnapshot(snapshot: any) {
 }
 
 // Save tasks to local storage whenever a change occurs
+// If logged in, save tasks to Firestore under the current user's document whenever a change occurs
 if (typeof window !== "undefined") {
   onSnapshot(taskStore, (snapshot) => {
     localStorage.setItem("taskStore", JSON.stringify(snapshot));
+    if (user) {
+      snapshot.tasks.forEach((task: any) => {
+        updateDoc(doc(db, "users", user.id, "tasks", task.id), task);
+      });
+    }
   });
 }
